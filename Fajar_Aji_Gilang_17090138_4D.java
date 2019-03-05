5c6ce1af4e5af46a7ebd00eb140adbab1d96feed
fajarajigilang <48247314+fajarajigilang@users.noreply.github.com>
2019-03-05 19:54:47 +0700

    Add files via upload

---
+++ Fajar_Aji_Gilang_17090138_4D.java
@@ -0,0 +1,8 @@
+
+package output;
+
+public class Fajar_Aji_Gilang_17090138_4D {
+    public static void main(String[] args) {
+        System.out.println("selamat datang di object oriented programing \n Fajar_aji_gilang_17090138_4D");
+    }
+    }
